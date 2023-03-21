da3ff0ee50a57fca55f50a1c4fa8dff6b64e6e74
zoro <stanjack0305@gmal.com>
2023-03-21 14:31:59 +0800

    [publish]: v0.0.1

---
+++ __version__.py
@@ -0,0 +1,4 @@
+__title__ = "workplace_safety"
+__description__ = 'workplace_safety'
+__version__ = "0.0.1"
+__author__ = "Zoro"
---
+++ scripts/Home.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   Home.py
+@Time    :   2023/03/21 10:18:21
+@Author  :   Zoro Chang
+'''
+
+import streamlit as st
+
+def main():
+    st.title("職災ㄨㄚ熊災－職場安全資訊攏底加！")
+
+main()
